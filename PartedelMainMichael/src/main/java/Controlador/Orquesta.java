fd1330f0e27a1540066d3c4acc222525897469cb
JoséAraya <arayacastilloj@gmail.com>
2020-12-06 21:14:38 -0600

    lista de instrumentos

--- PartedelMainMichael/src/main/java/Controlador/Orquesta.java
+++ PartedelMainMichael/src/main/java/Controlador/Orquesta.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,11 +12,12 @@
  * @author michael
  */
  
-import Controlador;
+//import Controlador;
 
 public class Orquesta {
+    ArrayList<Instrumento> listaInstrumentos = new ArrayList<Instrumento>();
 
-    public void agregarInstrumentoCuerda(String nombre, Estados estado, int cantidadCuerdas){
+    public void agregarInstrumentoCuerda(String nombre, Estado estado, int cantidadCuerdas){
     
     }
     
