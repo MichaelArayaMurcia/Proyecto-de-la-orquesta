ad7017ba1f03fd8b8b0b6682481a5affbbc85e91
MichaelArayaMurcia <michaelarayamurcia@gmail.com>
2020-12-06 22:48:58 -0600

    Creando los métodos de agregar instrumentos y romper instrumentos.

--- PartedelMainMichael/src/main/java/Controlador/Orquesta.java
+++ PartedelMainMichael/src/main/java/Controlador/Orquesta.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,8 +20,62 @@ public class Orquesta {
     private ArrayList<Instrumento> listaInstrumentos = new ArrayList<Instrumento>();
 
     public void agregarInstrumentoCuerda(String nombre, Estado estado, int cantidadCuerdas){
-    
+        Cuerda instrumento = new Cuerda(nombre, estado, cantidadCuerdas);
+        listaInstrumentos.add(instrumento);
     }
+    
+    public void agregarInstrumentoVientoMadera(String nombre, Estado estado, String clasificacion){
+        Viento_madera instrumento = new Viento_madera(nombre, estado, clasificacion);
+        listaInstrumentos.add(instrumento);
+    }
+    
+    public void agregarInstrumentoVientoMetal(String nombre, Estado estado, String clasificacion){
+        Viento_metal instrumento = new Viento_metal(nombre, estado, clasificacion);
+        listaInstrumentos.add(instrumento);
+    }    
+    
+    public void agregarInstrumentoPercusion(String nombre, Estado estado, Boolean baquetas){
+        Percusion instrumento = new Percusion(nombre, estado, baquetas);
+        listaInstrumentos.add(instrumento);
+    }
+    
+    public void conjuntoCuerda(String nombre, Estado estado, int cantidadCuerdas, int cantidad){
+        for(int i = 0; i < cantidad; i++){
+            Cuerda instrumento = new Cuerda(nombre, estado, cantidadCuerdas);
+            listaInstrumentos.add(instrumento);        
+        }
+
+    }
+    
+    public void conjuntoVientoMadera(String nombre, Estado estado, String clasificacion, int cantidad){
+        for(int i = 0; i < cantidad; i++){
+            Viento_madera instrumento = new Viento_madera(nombre, estado, clasificacion);
+            listaInstrumentos.add(instrumento);
+        }
+    }
+    
+    public void conjuntoVientoMetal(String nombre, Estado estado, String clasificacion, int cantidad){
+        for(int i = 0; i < cantidad; i++){
+            Viento_metal instrumento = new Viento_metal(nombre, estado, clasificacion);
+            listaInstrumentos.add(instrumento);
+        }
+
+    }    
+        
+    public void conjuntoPercusion(String nombre, Estado estado, Boolean baquetas, int cantidad){
+        for(int i = 0; i < cantidad; i++){
+            Percusion instrumento = new Percusion(nombre, estado, baquetas);
+            listaInstrumentos.add(instrumento);
+        }
+    }
+    
+    public void romperInstrumento(){
+        Random rand = new Random();
+        int numero = rand.nextInt(listaInstrumentos.size());
+        
+        listaInstrumentos.get(numero).setEstado(Estado.malo);
+    }
+    
     public void interpretarPieza(String interpretacion) throws Exception{
         for (int i = 0; i < listaInstrumentos.size(); i++) {
             Instrumento temp = listaInstrumentos.get(i);
